1f3e2d3a196fcdb2cf4b205617a58e25a4d5be66
Greg Hall <greghall@bantling.me>
2021-02-14 23:50:17 -0400

    Node tests

--- internal/parser/nodes_test.go
+++ internal/parser/nodes_test.go
@@ -1,15 +1,27 @@
 package parser
 
-//
-//import (
-//	"fmt"
-//	"io"
-//	"strings"
-//	"testing"
-//
-//	"github.com/stretchr/testify/assert"
-//)
-//
-//func TestTerminal(t *testing.T) {
-//	node :=
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerminal(t *testing.T) {
+	lex := "single \\\\ \\t \\r \\n \\' \" quoted"
+	str := "single \\ \t \r \n ' \" quoted"
+	term := OfTerminalString(lex, str)
+	assert.True(t, term.IsString())
+	assert.False(t, term.IsRange())
+	assert.Equal(t, str, term.TerminalString())
+	assert.Equal(t, map[rune]bool(nil), term.TerminalRange())
+	assert.Equal(t, lex, term.String())
+
+	lex = "[A-C]"
+	rng := map[rune]bool{'A': true, 'B': true, 'C': true}
+	term = OfTerminalRange(lex, rng)
+	assert.False(t, term.IsString())
+	assert.True(t, term.IsRange())
+	assert.Equal(t, "", term.TerminalString())
+	assert.Equal(t, rng, term.TerminalRange())
+	assert.Equal(t, lex, term.String())
+}
